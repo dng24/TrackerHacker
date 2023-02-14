fb7a9825689f9a83bae2e54cc5e0530d6c25fbee
Harrison-Dolin <dolinharry1@gmail.com>
2023-02-14 16:56:05 -0500

    updated with mitm

--- src/collectTraffic.py
+++ src/collectTraffic.py
@@ -2,6 +2,9 @@ from enum import Enum
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+import os
+
+
 
 class WebBrowsers(Enum):
     CHROME = "chrome"
@@ -17,4 +20,9 @@ def open_websites(urls, browsers):
         for url in urls:
             driver.get(url)
 
+def load_proxy(file):
+    os.system(f"mitmdump -s {}", file)
+
+
 open_websites(["https://duckduckgo.com"], [WebBrowsers.FIREFOX])
+
