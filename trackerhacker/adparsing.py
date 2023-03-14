06726f102c03625b85be7ef7ae9568a19a4eb153
Harrison-Dolin <dolinharry1@gmail.com>
2023-03-14 00:07:15 -0400

    added integration with trackerObject, added default and custom adtrack functionality, added processing of all adtracklists

--- trackerhacker/adparsing.py
+++ trackerhacker/adparsing.py
@@ -1,34 +1,60 @@
-#import adblock
-#import braveblock
 from adblockparser import AdblockRules
 
 
-#f = open('easylist.txt', 'r')
-#FILTER_LIST = f.read()
-#f.close()
 
-def parse_fqdns(logger, collected_list, domain_file):
+def parse_fqdns(logger, collected_list, adtrack_path, default_flag):
     #filter_set = adblock.FilterSet()
     #filter_set.add_filter_list(FILTER_LIST)
     #engine = adblock.Engine(filter_set=filter_set)
 
+    raw_rules = []
+    default_adtrack_lists = ['adlists/default_list.txt']
 
-    #AdBlockRules
+    if default_flag:
 
-    f = open(domain_file, 'r')
-    raw_rules = f.readlines()
+    #Default AdBlockRules
+        for lst in default_adtrack_lists:
+            print(f"this is the list: {lst}")
+            try:
+                f = open(lst, 'r')
+
+                tmp = f.readlines()
+                raw_rules.extend(tmp)
+                f.close()
+            except:
+                print("problem referencing a default file")
+                continue
+
+    print(f"this is the custom list path {adtrack_path}")
+
+    if adtrack_path != "":
+        try:
+            f = open(adtrack_path, 'r')
+
+            tmp = f.readlines()
+            raw_rules.extend(tmp)
+            f.close()
+        except:
+            print("problem opening custom adtracklist file")
+
     
     #Creates engine instance
     rules = AdblockRules(raw_rules)
-    #rules = AdblockRules(["https://adservice.google.com"])
-    f.close()
 
-    #BraveBlock
-    #bblocker = braveblock.Adblocker(
-    #    rules = raw_rules
-    #)
-        
 
+    #try:
+    #    cf = open(filepath, "r")
+    #except:
+    #    print("\nOops! Looks like there was a problem referencing the file. Make sure you entered the path correctly and the file is a txt file.")
+            
+    #for url in cf:
+    #    try:
+    #        custom_blocklist.append(url.strip())
+    #    except Exception:
+    #        print("Oops, looks like something is wrong with the default list file, and an error occured when processing it. Please make sure it is in the proper directory and the right format.\n")
+
+    #custom_raw_rules= AdblockRules(custom_blocklist)
+    #cf.close()
 
     #blockers = ["ad", "ads", "analytic", "tag", "tags", "pixel", "pxl", "px", "pix", "beacon", "metrics", "smetrics", "tracking", "track", "tracker", "sync"]
 
@@ -42,38 +68,3 @@ def parse_fqdns(logger, collected_list, domain_file):
                     blockresult = rules.should_block(full_url)
                     print(f"{full_url}:   ", blockresult)
         
-        #Adblockparser
-
-        #blockresult = rules.should_block(u)   
-        #print(f"{u} W/o https head:  ", blockresult)
-       
-        #Manual check for custom params   
-        #blockresult = any(sd in blockers for sd in u.split("."))
-        
-        #Adblockparser engine check against list
-        #blockresult = rules.should_block(u) #Only returns correct results for some ads, not all
-        #print(f"AdblockParser {u}:    ", blockresult)
-
-
-
-        #Braveblock
-
-        #blockresult = bblocker.check_network_urls(
-        #        url = u,
-        #        source_url = "https://www.google.com",
-        #        request_type = "", #No variation on request type seems to yield results
-        #)
-
-        #print(f"Braveblock {u}:   ", blockresult)
-
-
-        
-        #blockresult = engine.check_network_urls(
-        #        url = u,
-        #        source_url = "https://google.com",
-        #        request_type = "other"
-        #    )
-        #print(blockresult)
-
-
-#parse_fqdns('c', ["adservice.google.com", "a2.adform.net/", "www.hostg.xyz/", "c1.adform.net", "2mdn.net"], '/home/hson/Desktop/TrackerHacker/adlists/easylist.txt')
