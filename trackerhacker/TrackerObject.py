f4894ae0bdd12f2dafccf3c98c30d356ce7583e9
Harrison-Dolin <dolinharry1@gmail.com>
2023-03-15 21:55:13 -0400

    updated to remove default flag, custom adlists param

--- trackerhacker/TrackerObject.py
+++ trackerhacker/TrackerObject.py
@@ -1,7 +1,6 @@
 class TrackerObject:
-    def __init__(self, datapoints, browsers, query_urls, adtrack_blocklists, headless):
+    def __init__(self, datapoints, browsers, query_urls, headless):
         self.datapoints = datapoints
         self.browsers = browsers
         self.query_urls = query_urls
-        self.adtrack_blocklists = adtrack_blocklists
         self.headless = headless 
